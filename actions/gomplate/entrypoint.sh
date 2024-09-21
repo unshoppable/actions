2215fa476911c42a17885484710d12e5c2850468
Sascha Grüter <grueter.sascha@gmail.com>
2024-09-21 17:41:34 +0200

    fix() remove printenv from gomplate

--- actions/gomplate/entrypoint.sh
+++ actions/gomplate/entrypoint.sh
@@ -4,8 +4,6 @@ TEMPLATE=$1
 OUTPUT=$2
 VALUES=$3
 
-export $(printenv | sed 's/^\(.*\)$/\1/g')
-
 if [ -n "$VALUES" ]; then
   echo "$VALUES" > /tmp/values.yml
   gomplate -f "$TEMPLATE" -o "$OUTPUT" --datasource values=/tmp/values.yml
